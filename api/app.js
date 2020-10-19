9510edec2ba5f786e2df654c9f48704e160f3463
DairoFuentes7 <dairofuentes94@gmail.com>
2020-10-18 18:16:38 -0600

    Agregando Controladores de Albums

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var app = express();
 // cargar rutas
 var user_routes=require('./routes/user');
 var song_routes=require('./routes/song');
+var album_routes=require('./routes/album');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -25,7 +26,7 @@ app.use((req, res, next) => {
 //rutas base
 app.use('/api',user_routes);
 app.use('/api',song_routes);
-
+app.use('/api',album_routes);
 
 
 
